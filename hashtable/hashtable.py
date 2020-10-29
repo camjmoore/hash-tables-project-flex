386db2811e012f93a8978f4db9279174cf0d2b1c
Cameron Moore <cmrnmr@gmail.com>
2020-10-28 22:52:29 -0400

    implemented djb2

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -63,7 +63,12 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        hash = 5381  # prime number
+        input_to_bytes = key.encode()
+
+        for byte in input_to_bytes:
+            hash = ((hash << 5) + hash) + byte
+        return hash
 
     def hash_index(self, key):
         """
